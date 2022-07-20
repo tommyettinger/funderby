49e363ccd07d3e8b3926827ad0b296015c860011
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-07-20 01:59:28 -0700

    Consumers were also easy.

---
+++ src/main/java/com/github/tommyettinger/function/CharConsumer.java
@@ -0,0 +1,17 @@
+package com.github.tommyettinger.function;
+
+/**
+ * Represents an operation that accepts a single {@code char}-valued argument and
+ * returns no result.
+ * <br>
+ * This is a functional interface whose functional method is {@link #accept(char)}.
+ */
+@FunctionalInterface
+public interface CharConsumer {
+  /**
+   * Performs this operation on the given argument.
+   *
+   * @param value the input argument
+   */
+  void accept(char value);
+}
---
+++ src/main/java/com/github/tommyettinger/function/ShortConsumer.java
@@ -0,0 +1,17 @@
+package com.github.tommyettinger.function;
+
+/**
+ * Represents an operation that accepts a single {@code short}-valued argument and
+ * returns no result.
+ * <br>
+ * This is a functional interface whose functional method is {@link #accept(short)}.
+ */
+@FunctionalInterface
+public interface ShortConsumer {
+  /**
+   * Performs this operation on the given argument.
+   *
+   * @param value the input argument
+   */
+  void accept(short value);
+}
